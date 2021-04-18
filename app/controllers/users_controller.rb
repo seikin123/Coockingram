51a5bc0fd1ce2483eb33275c1215132baedb55c1
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-18 18:02:40 +0900

    [add]profile編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,14 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
-  
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :profile, :profile_iamge)
+  end
+
 end
