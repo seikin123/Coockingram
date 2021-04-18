dda865359fa3b130e556d4220d3f69c8a97d4c77
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-18 17:45:36 +0900

    [add]show/editアクション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,14 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+  end
+  
 end
