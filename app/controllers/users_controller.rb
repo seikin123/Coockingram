6e81718bbe5bda6a930f6c16eed5eebc8b7b64bd
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-09 00:54:03 +0900

    [add]recipes controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]) #一人のユーザーの情報のみ持ってくる
   end
 
   def edit
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]) #id番号さえわかれば良い どのユーザーかを見つけてくる
     @user.update(user_params)
     redirect_to user_path(@user)
   end
