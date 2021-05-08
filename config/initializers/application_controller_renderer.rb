a515ab858c15579115ca4518c33ef7841d8661bd
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-09 01:40:35 +0900

    [解決]runtime　key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3c457869ad0c914987758cbf5c9162ef8d3e20531aab4797ce7a379aa7c3b4f4bbc1dbff25db5da46f1760c559e2039b9cc1ae9bba2660a62248df2b6caa3c3c'
+
+
