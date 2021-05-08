8d13e780e955309d0c95257d10f0b398840ccc54
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-09 01:39:23 +0900

    [add]投稿機能

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,11 +3,25 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find(params[:id]) #URLのidから番号を持ってきてくれる
   end
 
   def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id #ユーザーidのところに今ログインしている人の情報が入って、user_idが保持される
+    @recipe.save
+    redirect_to recipe_path(@recipe)
   end
 
   def edit
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :body, :image)
   end
 end
