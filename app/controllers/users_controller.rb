b5aec84e2b788d29e075a5e719e1d1390cde6ffe
ヨシダタイキ <abc521fjr7@gmail.com>
2021-08-25 14:56:21 +0900

    [add]alert、indexのみ許可

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index]
   def following
     #@userがフォローしているユーザー
     @user  = User.find(params[:id])
@@ -24,12 +24,15 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+        redirect_to user_path(current_user), alert: "不正なアクセスです。"
+    end
   end
 
   def update
     @user = User.find(params[:id]) #id番号さえわかれば良い どのユーザーかを見つけてくる
     @user.update(user_params)
-    redirect_to user_path(@user)
+    redirect_to user_path(@user), notice: "ユーザー情報を更新しました。"
   end
 
   private
