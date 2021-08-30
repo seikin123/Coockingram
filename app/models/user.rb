22c89b6c53358dc5259b2f8ecf8d5b47ec39cd2e
ヨシダタイキ <abc521fjr7@gmail.com>
2021-08-31 01:18:30 +0900

    [add]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :recipes, dependent: :destroy
 
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロワー取得
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_160859) do
+ActiveRecord::Schema.define(version: 2021_08_30_161603) do
 
   create_table "recipes", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
     t.text "body"
     t.string "image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
