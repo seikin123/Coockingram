2a56182ca2f0f1cea3e8295e2388e10136e932b5
ヨシダタイキ <abc521fjr7@gmail.com>
2021-08-31 01:06:28 +0900

    [delete]relationships

--- app/models/relationship.rb
+++
@@ -1,8 +0,0 @@
-class Relationship < ApplicationRecord
-    belongs_to :follower, class_name: "User"
-    #自分がフォローしているユーザー
-    belongs_to :following, class_name: "User"
-    #バリデーション
-    validates :follower_id, presence: true
-    validates :following_id, presence: true
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,23 +6,4 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :recipes, dependent: :destroy
 
-    has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship",  dependent: :destroy
-    has_many :following, through: :following_relationships
-    has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
-    has_many :followers, through: :follower_relationships
 
-  #フォローしているかを確認するメソッド
-  def following?(user)
-    following_relationships.find_by(following_id: user.id)
-  end
-
-  #フォローするときのメソッド
-  def follow(user)
-    following_relationships.create!(following_id: user.id)
-  end
-
-  #フォローを外すときのメソッド
-  def unfollow(user)
-    following_relationships.find_by(following_id: user.id).destroy
-  end
-end
