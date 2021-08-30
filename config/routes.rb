38ab2cf57723a76178a270fa4d17272968e490e9
ヨシダタイキ <abc521fjr7@gmail.com>
2021-08-31 01:23:40 +0900

    [addフォロー機能]ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :recipes
-  resources :users do
-    member do
-      get :following, :followers
-  end
-end
+  resources :users
+
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  
 resources :relationships, only: [:create, :destroy]
 
 end
