7ab38cee3a419dcaa663be800197775d98855559
ヨシダタイキ <abc521fjr7@gmail.com>
2021-08-31 23:32:28 +0900

    [add]いいね の表示

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   attachment :image
+
+  has_many :likes, dependent: :destroy
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
